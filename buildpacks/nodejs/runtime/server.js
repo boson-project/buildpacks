2b2a51520d77ffd71467c61d8cb03dbdd262f4c3
Lance Ball <lball@redhat.com>
2020-09-01 15:15:49 -0400

    buildpacks(node): bump dependency on faas-js-runtime

--- buildpacks/nodejs/runtime/server.js
+++ buildpacks/nodejs/runtime/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const framework = require('@redhat/faas-js-runtime');
+const ON_DEATH = require('death')({ uncaughtException: true });
 
 const functionPath = process.env.FUNCTION_PATH || '../';
 const LISTEN_PORT = process.env.LISTEN_PORT || 8080;
@@ -15,6 +16,7 @@ try {
   framework(require(functionPath), LISTEN_PORT, srv => {
     console.log('FaaS framework initialized');
     server = srv;
+    ON_DEATH(_ => { server.close(); });
   });
 } catch (err) {
   console.error(`Cannot load user function at ${functionPath}`);
