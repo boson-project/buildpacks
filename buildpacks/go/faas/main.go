c4589714d41f9d0759b6c8b5d4fa922f4ec7cf52
Matej Vasek <mvasek@redhat.com>
2020-09-08 16:19:44 +0200

    feat: raw HTTP support for Go

--- buildpacks/go/faas/main.go
+++ buildpacks/go/faas/main.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 
@@ -20,7 +24,7 @@ var (
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, usage)
+		fmt.Fprintln(os.Stderr, usage)
 		flag.PrintDefaults()
 	}
 	parseEnv()   // override static defaults with environment.
@@ -33,23 +37,87 @@ func main() {
 
 // run a cloudevents client in receive mode which invokes
 // the user-defined function.Handler on receipt of an event.
-func run() (err error) {
-	transport, err := cloudevents.NewHTTP(
-		cloudevents.WithPort(*port),
-		cloudevents.WithPath("/"))
-	if err != nil {
-		return
+func run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		cancel()
+	}()
+
+	var handler interface{} = function.Handle
+
+	httpHandler := toHttpHandler(handler, ctx)
+
+	if httpHandler != nil {
+		httpServer := &http.Server{
+			Addr:           fmt.Sprintf(":%d", *port),
+			Handler:        httpHandler,
+			ReadTimeout:    1 * time.Minute,
+			WriteTimeout:   1 * time.Minute,
+			MaxHeaderBytes: 1 << 20,
+		}
+
+		listenAndServeErr := make(chan error, 1)
+		go func() {
+			if *verbose {
+				fmt.Printf("listening on http port %v for HTTP requests\n", *port)
+			}
+			err := httpServer.ListenAndServe()
+			cancel()
+			listenAndServeErr <- err
+		}()
+
+		<- ctx.Done()
+		shutdownCtx, shutdownCancelFn := context.WithTimeout(context.Background(), time.Second * 5)
+		defer shutdownCancelFn()
+		err := httpServer.Shutdown(shutdownCtx)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error on server shutdown: %v\n", err)
+		}
+
+		err = <-listenAndServeErr
+		if http.ErrServerClosed == err {
+			return nil
+		}
+		return err
+	} else {
+		transport, err := cloudevents.NewHTTP(
+			cloudevents.WithPort(*port),
+			cloudevents.WithPath("/"))
+		if err != nil {
+			return err
+		}
+		client, err := cloudevents.NewClient(transport)
+		if err != nil {
+			return err
+		}
+		if *verbose {
+			fmt.Printf("listening on http port %v for JSON-encoded CloudEvents\n", *port)
+		}
+		return client.StartReceiver(ctx, handler)
 	}
-	client, err := cloudevents.NewClient(transport)
-	if err != nil {
-		return
+
+}
+
+// if the handler signature is compatible with http handler the function returns an instance of `http.Handler`,
+// otherwise nil is returned
+func toHttpHandler(handler interface{}, ctx context.Context) http.Handler {
+
+	if f, ok := handler.(func(rw http.ResponseWriter, req *http.Request)); ok {
+		return http.HandlerFunc(f)
 	}
-	if *verbose {
-		fmt.Printf("listening on http port %v for JSON-encoded CloudEvents\n", *port)
+
+	if f, ok := handler.(func(ctx context.Context, rw http.ResponseWriter, req *http.Request)); ok {
+		ff := func(rw http.ResponseWriter, req *http.Request) {
+			f(ctx, rw, req)
+		}
+		return http.HandlerFunc(ff)
 	}
-	err = client.StartReceiver(context.Background(), function.Handle)
-	// TODO: wait for signal, cancel receiver context, and wait for graceful shutdown.
-	return
+
+	return nil
 }
 
 // parseEnv parses environment variables, populating the destination flags
