97fd2de75fae4f2a64b8ed4a383b440a11056140
Evan Shortiss <evanshortiss@gmail.com>
2021-04-09 19:45:25 +0100

    feat: support configurable log levels for node.js functions

--- buildpacks/nodejs/runtime/server.js
+++ buildpacks/nodejs/runtime/server.js
@@ -5,11 +5,14 @@ const ON_DEATH = require('death')({ uncaughtException: true });
 
 const functionPath = process.env.FUNCTION_PATH || '../';
 const LISTEN_PORT = process.env.LISTEN_PORT || 8080;
+const FUNCTION_LOG_LEVEL = process.env.FUNCTION_LOG_LEVEL || 'info'
 
 try {
   framework(require(functionPath), LISTEN_PORT, server => {
     console.log('FaaS framework initialized');
     ON_DEATH(_ => { server.close(); });
+  }, {
+    logLevel: FUNCTION_LOG_LEVEL
   });
 } catch (err) {
   console.error(`Cannot load user function at ${functionPath}`);
