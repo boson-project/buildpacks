8d2ad86c616c70dfef24ebed2b442c0f12ac004d
Lance Ball <lball@redhat.com>
2022-01-20 15:35:49 -0500

    fix: update test binaries with knative-sandbox (#125)
    Signed-off-by: Lance Ball <lball@redhat.com>

--- test/install_func_stable.sh
+++ test/install_func_stable.sh
@@ -8,5 +8,5 @@ WD="$(pwd)"
 mkdir -p "$WD/bin/"
 
 # install latest official release
-curl -L -o - https://github.com/boson-project/func/releases/latest/download/func_linux_amd64.gz | gunzip > "$WD/bin/func_stable"
+curl -L -o - https://github.com/knative-sandbox/kn-plugin-func/releases/latest/download/func_linux_amd64 > "$WD/bin/func_stable"
 chmod 755 "$WD/bin/func_stable"
